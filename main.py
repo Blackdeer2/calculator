1e2167a3164097e527e00a6206b45e4612542a1b
Володя Висоцький <74934018+Blackdeer2@users.noreply.github.com>
2024-10-02 14:48:54 +0300

    go to

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-import functions
-import app_settings
-from classes.calculator import Calculator
+
 
 def main():
+    print("main")
 
-    calculator = Calculator()
-    while True:
-        functions.show_menu()
-        choice = input('Enter your choice: ').strip()
-
-        match choice:
-            case '1':
-                calculator.run()
-            case '2':
-                app_settings.setting()
-            case '0':
-                print("Exiting the program. Goodbye!")
-                break
-            case _:
-                print("Invalid choice. Please try again.")
 
 
 main()
