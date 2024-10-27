a46b78bd937bb8d710d32d6285c3275784ca5408
Володя Висоцький <74934018+Blackdeer2@users.noreply.github.com>
2024-10-27 17:10:00 +0200

    did 1,2,4,6,7,8,9,10 tasks

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from BLL.classes.AsciiArtGenerator import AsciiArtGenerator
-import pyfiglet
-import art
+from BLL.classes.MyAsciiArtGenerator import MyAsciiArtGenerator
+from Sources.ABC123hash import draw_char_hash
+from Sources.ABC123dog import draw_char_dog
+from Sources.ABC123asterism import draw_char_asterism
+
+def figlet_format(art_text, font):
+            result = [""] * 7  
+
+            for letter in art_text:
+                if font == "#":
+                    letter_art = draw_char_hash(letter)
+                elif font == "@":
+                    letter_art = draw_char_dog(letter)
+                elif font == "*":
+                    letter_art = draw_char_asterism(letter)
+                
+                for i in range(7):
+                    result[i] += letter_art[i]  
+
+            return "\n".join(result)
+            # for line in result:
+            #     print(line)
+            # print()
+
+
+
 
 def main():
 
-    art = AsciiArtGenerator()
+    art = MyAsciiArtGenerator()
     art.run()
+    # lol = figlet_format("ABC123", "#")
+
+    # print(lol)
 
 
 main()
